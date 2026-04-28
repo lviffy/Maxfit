04aeda167d88914d102b8c96170762ee50a48320
lviffy <contact.rohan.here@gmail.com>
2026-04-29 01:51:43 +0530

    cors

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,11 @@ class CustomJSONProvider(DefaultJSONProvider):
 app = Flask(__name__)
 app.json_provider_class = CustomJSONProvider
 app.json = CustomJSONProvider(app)
-CORS(app, supports_credentials=True)
+CORS(
+    app,
+    resources={r"/*": {"origins": ["http://localhost:3000"]}},
+    supports_credentials=True
+)
 app.secret_key = 'super_secret_gym_key' # Needed for session tracking
 
 def get_db_connection():
